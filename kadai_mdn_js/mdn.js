0ffb6f4b7ef4a8efdb0cce74c9a28d271acde435
MIKErinji <hikawamaru12266@gmail.com>
2025-10-04 12:38:48 +0900

    1回目のコミット

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+const today = new Date();
+
+const day = today.getDate();
+
+const month = today.getMonth();
+
+const year = today.getFullYear();
+
+console.log( year + '年' + ( month + 1 ) + '月' + day +'日') ;
+
