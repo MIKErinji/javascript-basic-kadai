227f5d4895c50c1fe9f30922b36a517550e89360
MIKErinji <hikawamaru12266@gmail.com>
2025-10-05 10:59:00 +0900

    １回目のコミット

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+const doButton = document.getElementById('btn');
+
+doButton.addEventListener('click', () => {
+
+  const newText = document.getElementById('text');
+
+  newText.textContent ='ボタンをクリックしました';
+
+
+});
